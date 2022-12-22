ea462ec6c2451590335c5d0c3a6f5b0bc87ac177
DorongSeok <71745818+DorongSeok@users.noreply.github.com>
2022-12-22 16:46:36 +0900

    feat: Add Trail Effect

--- Project_Flash/Assets/D/Scripts/CharacterMove.cs
+++ Project_Flash/Assets/D/Scripts/CharacterMove.cs
@@ -4,21 +4,77 @@ using UnityEngine;
 
 public class CharacterMove : MonoBehaviour
 {
-    public int speed;
-
     private Rigidbody2D rigidBody;
-    private Vector2 vector;
+    private float directionX;
+    private float directionY;
+    private bool isMove;
 
-    private void Start()
+    public float moveSpeed = 1500;
+    public float delayTime = 0.2f;
+
+    private void Awake()
     {
+        isMove = false;
         rigidBody = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
-        vector.x = Input.GetAxisRaw("Horizontal");
-        vector.y = Input.GetAxisRaw("Vertical");
+        directionX = Input.GetAxisRaw("Horizontal"); // 좌우 입력
+        directionY = Input.GetAxisRaw("Vertical"); // 상하 입력
 
-        rigidBody.velocity = vector.normalized * speed;
+        if (Input.GetKeyDown(KeyCode.Space) && isMove == false)
+        {
+            Move();
+        }
+    }
+    private void Move()
+    {
+        if (directionX == 0 && directionY == 0) // 오른쪽 위 대각선
+        {
+            return;
+        }
+        else
+        {
+            StartCoroutine(nameof(MoveDelay));
+            if (directionX == 1 && directionY == 0) // 오른쪽
+            {
+                rigidBody.AddForce((Vector2.right.normalized * moveSpeed), ForceMode2D.Force);
+            }
+            else if (directionX == 1 && directionY == 1) // 오른쪽 위 대각선
+            {
+                rigidBody.AddForce(((Vector2.right + Vector2.up).normalized * moveSpeed), ForceMode2D.Force);
+            }
+            else if (directionX == 0 && directionY == 1) // 위쪽
+            {
+                rigidBody.AddForce((Vector2.up.normalized * moveSpeed), ForceMode2D.Force);
+            }
+            else if (directionX == -1 && directionY == 1) // 왼쪽 위 대각선
+            {
+                rigidBody.AddForce(((Vector2.left + Vector2.up).normalized * moveSpeed), ForceMode2D.Force);
+            }
+            else if (directionX == -1 && directionY == 0) // 왼쪽
+            {
+                rigidBody.AddForce((Vector2.left.normalized * moveSpeed), ForceMode2D.Force);
+            }
+            else if (directionX == -1 && directionY == -1) // 왼쪽 아래 대각선
+            {
+                rigidBody.AddForce(((Vector2.left + Vector2.down).normalized * moveSpeed), ForceMode2D.Force);
+            }
+            else if (directionX == 0 && directionY == -1) // 아래쪽
+            {
+                rigidBody.AddForce((Vector2.down.normalized * moveSpeed), ForceMode2D.Force);
+            }
+            else if (directionX == 1 && directionY == -1) // 오른쪽 아래 대각선
+            {
+                rigidBody.AddForce(((Vector2.right + Vector2.down).normalized * moveSpeed), ForceMode2D.Force);
+            }
+        }
+    }
+    IEnumerator MoveDelay()
+    {
+        isMove = true;
+        yield return new WaitForSeconds(delayTime);
+        isMove = false;
     }
 }
