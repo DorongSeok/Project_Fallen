94af7c75fe11791545980e921d358284fd3da0d6
hyun soo <157648@naver.com>
2023-02-02 17:27:39 +0900

    feat : Update CameraMove and Update Section 2-2

--- Project_Flash/Assets/02.Scripts/Common/CameraMove.cs
+++ Project_Flash/Assets/02.Scripts/Common/CameraMove.cs
@@ -7,12 +7,12 @@ public class CameraMove : MonoBehaviour
 {
     private Transform camTr;
     private Transform targetTr;
-    private Camera cam;
-    private Vector3 movePos; // 움직일 목표 위치
+    //private Camera cam;
+    //private Vector3 movePos; // 움직일 목표 위치
 
-    private bool isMoving;
-    private bool camUp;
-    private bool camDown;
+    //private bool isMoving;
+    //private bool camUp;
+    //private bool camDown;
     
     public float camHeight = -10.0f;
 
@@ -23,58 +23,64 @@ public class CameraMove : MonoBehaviour
 
     private void Awake()
     {
-        isMoving = false;
-        cam = Camera.main;
+        //isMoving = false;
+        //cam = Camera.main;
         camTr = GetComponent<Transform>();
     }
     private void Update()
     {
         if (targetTr != null)
         {
-            movePos = (Vector3.up * targetTr.position.y) + (targetTr.forward * camHeight); // 카메라 이동 시, 이동 지점을 나타내는 값
-                                                                                           // 추후 3가지의 카메라 이동 중 1개를 선택해 적용할 것
-                                                                                           //camTr.position = Vector3.Slerp(camTr.position, movePos, Time.deltaTime * moveDamping); // 천천히 따라오기
-                                                                                           //camTr.position = movePos; // 즉시 따라오기
-            if (cam.WorldToScreenPoint(targetTr.position).y > (cam.WorldToScreenPoint(camTr.position).y * 1.6) && isMoving == false) // 스크린 중앙 점으로 시작해서 화면이 넘어가는 공간 까지의 차이를 비율로 나타내면 60%씩 차이남. 그에 따른 계산식이 1.6, 0.4
-            {
-                StartCoroutine(nameof(MoveUpCamera));
-            }
-            else if (cam.WorldToScreenPoint(targetTr.position).y < (cam.WorldToScreenPoint(camTr.position).y * 0.4) && isMoving == false)
-            {
-                StartCoroutine(nameof(MoveDownCamera));
-            }
+            // 추후 3가지의 카메라 이동 중 1개를 선택해 적용할 것
+
+
+            //movePos = (Vector3.up * targetTr.position.y) + (targetTr.forward * camHeight); // 3. 고정 지점 통과 시, 따라오기
+            //if (cam.WorldToScreenPoint(targetTr.position).y > (cam.WorldToScreenPoint(camTr.position).y * 1.6) && isMoving == false) // 스크린 중앙 점으로 시작해서 화면이 넘어가는 공간 까지의 차이를 비율로 나타내면 60%씩 차이남. 그에 따른 계산식이 1.6, 0.4
+            //{
+            //    StartCoroutine(nameof(MoveUpCamera));
+            //}
+            //else if (cam.WorldToScreenPoint(targetTr.position).y < (cam.WorldToScreenPoint(camTr.position).y * 0.4) && isMoving == false)
+            //{
+            //    StartCoroutine(nameof(MoveDownCamera));
+            //}
         }
         
     }
     private void LateUpdate()
     {
-        // PC 이동 방향에 따른 카메라 이동
-        if (camUp == true)
-        {
-            camTr.position = Vector3.Slerp(camTr.position, movePos + Vector3.up * upDownDamping, Time.deltaTime * moveDamping);
-        }
-        if (camDown == true)
-        {
-            camTr.position = Vector3.Slerp(camTr.position, movePos + Vector3.down * upDownDamping, Time.deltaTime * moveDamping);
-        }
+        //camTr.position = targetTr.position + (targetTr.forward * camHeight); // 1. 즉시 따라오기
+        
+        // 3번과 연계한 PC 이동 방향에 따른 카메라 이동
+        //if (camUp == true)
+        //{
+        //    camTr.position = Vector3.Slerp(camTr.position, movePos + Vector3.up * upDownDamping, Time.deltaTime * moveDamping);
+        //}
+        //if (camDown == true)
+        //{
+        //    camTr.position = Vector3.Slerp(camTr.position, movePos + Vector3.down * upDownDamping, Time.deltaTime * moveDamping);
+        //}
     }
-    IEnumerator MoveUpCamera() // 카메라 상승시킬 때 처리
+    // 3번과 연계한 카메라 이동
+    //IEnumerator MoveUpCamera() // 카메라 상승시킬 때 처리
+    //{
+    //    isMoving = true;
+    //    camUp = true;
+    //    yield return new WaitForSeconds(waitingTime);
+    //    isMoving = false;
+    //    camUp = false;
+    //}
+    //IEnumerator MoveDownCamera() // 카메라 하강시킬 때 처리
+    //{
+    //    isMoving = true;
+    //    camDown = true;
+    //    yield return new WaitForSeconds(waitingTime);
+    //    isMoving = false;
+    //    camDown = false;
+    //}
+    private void FixedUpdate()
     {
-        isMoving = true;
-        camUp = true;
-        yield return new WaitForSeconds(waitingTime);
-        isMoving = false;
-        camUp = false;
+        camTr.position = Vector3.Lerp(camTr.position, new Vector3(0, targetTr.position.y, camHeight), Time.deltaTime * moveDamping); // 2. 천천히 따라오기
     }
-    IEnumerator MoveDownCamera() // 카메라 하강시킬 때 처리
-    {
-        isMoving = true;
-        camDown = true;
-        yield return new WaitForSeconds(waitingTime);
-        isMoving = false;
-        camDown = false;
-    }
-
     public void SetTargetTr(Transform targetTr) // 카메라가 추적할 대상 세팅
     {
         this.targetTr = targetTr;
---
+++ Project_Flash/Assets/02.Scripts/Obstacle/Obstacle_BigAndSmall.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle_BigAndSmall : MonoBehaviour
+{
+    Vector3 scale;
+    public float delta;
+    public float speed;
+
+    public float delay;
+    private void Start()
+    {
+        scale = transform.localScale;
+    }
+    private void Update()
+    {
+        Vector3 v = scale;
+        v.x += Mathf.Abs(delta * Mathf.Sin(delay + (Time.time * speed)));
+        v.y += Mathf.Abs(delta * Mathf.Sin(delay + (Time.time * speed)));
+
+        transform.localScale = v;
+    }
+}
