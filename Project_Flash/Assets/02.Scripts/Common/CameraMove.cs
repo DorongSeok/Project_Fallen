7aa1c08f57c51d8963c90bf6e88fe14de5fc4541
hyun soo <157648@naver.com>
2023-05-09 22:30:50 +0900

    feat : Update CameraMove

--- Project_Flash/Assets/02.Scripts/Common/CameraMove.cs
+++ Project_Flash/Assets/02.Scripts/Common/CameraMove.cs
@@ -7,7 +7,9 @@ public class CameraMove : MonoBehaviour
 {
     private Transform camTr;
     private Transform targetTr;
+
     private bool isCameraUp = false;
+    private bool isFalling = false;
     
     public float camHeight = -10.0f;
 
@@ -23,6 +25,22 @@ public class CameraMove : MonoBehaviour
     {
         camTr = GetComponent<Transform>();
     }
+    private void Start()
+    {
+        PlayerCharacterControl.onPlayerFallingStart += this.StartFalling;
+        PlayerCharacterControl.onPlayerFallingEnd += this.EndFalling;
+
+        PlayerCharacterTest2.onPlayerFallingStart += this.StartFalling;
+        PlayerCharacterTest2.onPlayerFallingEnd += this.EndFalling;
+    }
+    private void OnDisable()
+    {
+        PlayerCharacterControl.onPlayerFallingStart -= this.StartFalling;
+        PlayerCharacterControl.onPlayerFallingEnd -= this.EndFalling;
+
+        PlayerCharacterTest2.onPlayerFallingStart -= this.StartFalling;
+        PlayerCharacterTest2.onPlayerFallingEnd -= this.EndFalling;
+    }
     private void Update()
     {
         if (Input.GetKey(KeyCode.B))
@@ -36,13 +54,13 @@ public class CameraMove : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        if (isCameraUp == false)
-        {
-            camTr.position = Vector3.Lerp(camTr.position, new Vector3(0, targetTr.position.y, camHeight), Time.deltaTime * moveDamping); // 2. 천천히 따라오기
-        }
-        else if (isCameraUp == true)
+        if (isCameraUp == true && isFalling == false)
         {
             camTr.position = Vector3.Slerp(camTr.position, new Vector3(0, targetTr.position.y + upPoint, camHeight), Time.deltaTime * upDamping);
+        }
+        else
+        {
+            camTr.position = Vector3.Lerp(camTr.position, new Vector3(0, targetTr.position.y, camHeight), Time.deltaTime * moveDamping); // 2. 천천히 따라오기
         }
     }
     public void SetTargetTr(Transform targetTr) // 카메라가 추적할 대상 세팅
@@ -53,5 +71,13 @@ public class CameraMove : MonoBehaviour
     public void SetCameraStartPos()
     {
         camTr.position = new Vector3(camTr.position.x, targetTr.position.y, camTr.position.z);
+    }
+    private void StartFalling()
+    {
+        isFalling = true;
+    }
+    private void EndFalling()
+    {
+        isFalling = false;
     }
 }
